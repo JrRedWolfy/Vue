b047ed5d15796100ae648170f7b8f6090760113b
acwef <Red>
2021-05-18 12:32:10 +0200

    Alleluya!

--- libros/Libro.js
+++ libros/Libro.js
@@ -3,7 +3,7 @@ let Libro = {
     template: `
         <div>
 
-            <div v-for="(libro, index) in libros" :key="index"
+            <div v-for="(libro, index) in allLibros" :key="index"
             >
             <expositor
             :title="libro.title"
@@ -30,16 +30,26 @@ let Libro = {
     methods: {
 
         getBooks(){
-            const URL = `https://www.etnassoft.com/api/v1/get/?keyword=&publisher_date=2011`
-            fetch(URL)
-            .then(response => response.json())
-            .then(data => {
-                this.libros=data
-                console.log(data[0])
-            })
 
-            this.allLibros= this.allLibros.concat(this.libros);
+            date = 10
 
+            do {
+                const URL = `https://www.etnassoft.com/api/v1/get/?keyword=&publisher_date=20${date}`
+                fetch(URL)
+                .then(response => response.json())
+                .then(data => {
+                    this.libros=data
+                    this.allLibros = this.allLibros.concat(data);
+                    console.log(this.allLibros)
+ //                   console.log(data)
+                })
+                date += 1
+                
+
+            } while (date < 20);
+
+           
+           
 
         }
 
